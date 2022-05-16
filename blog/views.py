993111619fcf0345a00202babe644aaae4cfbdde
gohanko <26451183+gohanko@users.noreply.github.com>
2022-05-16 16:10:34 +0800

    Render category in category dropdown

--- blog/views.py
+++ blog/views.py
@@ -2,21 +2,38 @@ from typing import List
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
-from blog.models import Tag, Post
+from blog.models import Category, Tag, Post
 
 class TagListView(ListView):
     model = Tag
+
+    def get_context_data(self, *args, **kwargs):
+        data = super().get_context_data(*args, **kwargs)
+        data['categories'] = Category.objects.all()
+        return data
 
 class PostWithTagList(ListView):
     template_name = 'blog/post_list.html'
 
     def get_context_data(self, *args, **kwargs):
-        pass
+        data = super().get_context_data(*args, **kwargs)
+        data['categories'] = Category.objects.all()
+        return data
 
 class PostDetailView(DetailView):
     model = Post
 
+    def get_context_data(self, *args, **kwargs):
+        data = super().get_context_data(*args, **kwargs)
+        data['categories'] = Category.objects.all()
+        return data
+
 class PostListView(ListView):
     model = Post
     paginate_by = 20
+
+    def get_context_data(self, *args, **kwargs):
+        data = super().get_context_data(*args, **kwargs)
+        data['categories'] = Category.objects.all()
+        return data
 
