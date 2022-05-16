6c4a35fb22c722acfd4e690a9cbdc716ae83558b
gohanko <26451183+gohanko@users.noreply.github.com>
2022-05-16 17:56:14 +0800

    stylize header and added side bar

--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,7 @@ class BaseView:
     def get_context_data(self, *args, **kwargs):
         data = super().get_context_data(*args, **kwargs)
         data['categories'] = Category.objects.all()
+        data['tags'] = Tag.objects.all()
         return data
 
 class TagListView(BaseView, ListView):
