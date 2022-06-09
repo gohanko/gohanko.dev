7ac5a26ee85358a13aabd287cce37f669943c59c
Yii Kuo Chong <byu1628@proton.me>
2022-06-10 03:28:24 +0800

    tests: addded boiletplate files

---
+++ app/blog/tests/views/test_post_view.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+
+class TestPostListView(TestCase):
+    pass
+
+class TestPostDetailView(TestCase):
+    pass
+
+class TestPostCreateview(TestCase):
+    def setUp(self):
+        pass
+
+    def test_login_required(self):
+        pass
+
+class TestPostUpdateView(TestCase):
+    pass
+
+class TestPostDeleteView(TestCase):
+    pass
